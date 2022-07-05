859d226a32db519b8d33b9c813319e63956c7662
ren <3028702465@qq.com>
2022-07-05 09:59:11 +0800

    feat(main and vue.config): 安装引入element与axios以及配置跨域

---
+++ src/views/login/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="login"></div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+.login {
+  background-color: #fafafa;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: true,
+  publicPath: './',
+  devServer: {
+    host: 'localhost',
+    port: 8888,
+    https: false,
+    open: false,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL, //跨域请求的公共地址
+        changeOrigin: true, //是否开启跨域，值为 true 就是开启， false 不开启
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: '' //注册全局路径， 但是在你请求的时候前面需要加上
+        }
+      }
+    }
+  }
 })
